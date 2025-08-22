8dc5ba971ebcedac6fa2ced6dadefb4f08964697
Paul Marshall <ptm@esarks.com>
2025-08-21 20:49:56 -0400

    sync

---
+++ AllowanceAlleyNextgen/AllowanceAlleyNextgenTests/AllowanceAlleyNextgenTests.swift
@@ -0,0 +1,17 @@
+//
+//  AllowanceAlleyNextgenTests.swift
+//  AllowanceAlleyNextgenTests
+//
+//  Created by Paul Marshall on 8/21/25.
+//
+
+import Testing
+@testable import AllowanceAlleyNextgen
+
+struct AllowanceAlleyNextgenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
